f1ce8403fe7d39f9e707c44c4df76912f7da0416
adam <edek-150@o2.pl>
2016-04-20 21:32:59 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers} from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory, Link, IndexRoute } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import { EditFormContainer } from './EditForm.jsx';
+
+const store = createStore(
+  combineReducers({
+    routing: routerReducer
+  }), {}, window.devToolsExtension ? window.devToolsExtension() : undefined
+);
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='app'>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Link to={'edit'} className=''>Create New</Link>
+      </div>
+    );
+  }
+}
+
+class Main extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        { /* Tell the Router to use our enhanced history */ }
+        <Router history={history}>
+          <Route path='/' component={App}>
+            <IndexRoute component={Home} />
+            <Route path='edit' component={EditFormContainer}/>
+          </Route>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
---
+++ src/EditForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class EditFormContainer extends React.Component {
+  render() {
+    return (
+      <h1>Foo</h1>
+    );
+  }
+}

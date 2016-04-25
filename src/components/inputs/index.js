bfd49346e99807a9824c6993f5a0046830530337
adam <edek-150@o2.pl>
2016-04-25 13:50:33 +0200

    index for export inputs

---
+++ src/components/inputs/index.js
@@ -0,0 +1,6 @@
+import TextInput from './TextInput.jsx';
+import CheckboxInput from './CheckboxInput.jsx';
+import SelectInput from './SelectInput.jsx';
+import TextareaInput from './TextareaInput.jsx';
+
+export {TextInput, CheckboxInput, SelectInput, TextareaInput};
